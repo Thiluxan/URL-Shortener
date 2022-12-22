74ffae47c45e626a77b4d704efce57ec524641ba
Sivanujan1998 <ssivanujan1998@gmail.com>
2022-12-22 23:59:59 +0530

    logic created to determine whether or not the app is working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use("/health",require("./routes/health"))
+
 const PORT = 3000 || process.env.PORT
 app.listen(PORT, () => console.log("Application Started on ",PORT))
